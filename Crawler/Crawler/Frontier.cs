148dddf2432da93aae2e180afee5e6c56fbb62c2
Naresh Kumar <nareshkumar66675@gmail.com>
2018-04-15 17:57:52 -0500

    Frontier Queue

---
+++ Crawler/Crawler/Frontier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    public class URLData
+    {
+        public string URL { get; set; }
+
+        public int Hit { get; set; } 
+
+        public int Hierarchy { get; set; }
+
+        public void IncreaseHit()
+        {
+            Hit++;
+        }
+
+        public void IncreaseHierarchy()
+        {
+            Hierarchy++;
+        }
+    }
+    public static class Frontier
+    {
+        public static ConcurrentDictionary<string, URLData> CurrentQueue { get; set; }
+
+        public static ConcurrentDictionary<string, URLData> CompletedQueue { get; set; }
+
+        static Frontier()
+        {
+            CurrentQueue = new ConcurrentDictionary<string, URLData>(Environment.ProcessorCount * 2, 200);
+            CompletedQueue = new ConcurrentDictionary<string, URLData>(Environment.ProcessorCount * 2, 10000);
+        }
+         
+    }
+}
--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,8 +1,10 @@
 ﻿using DocumentParser;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,8 @@ namespace Crawler
     {
         static void Main(string[] args)
         {
+            //var a= Environment.ProcessorCount;
+            RetrieveHTML();
             Parser parser = new Parser();
 
             string text = string.Empty;//= File.ReadAllText(@"C:\Users\Naresh\Downloads\docsnew\docsnew\Atlanta_Falcons_seasons.htm");
@@ -24,9 +28,30 @@ namespace Crawler
                 File.WriteAllText(Path.Combine(@"C:\Users\Naresh\Desktop\HTML", Path.GetFileName(t)), result);
             });
 
-
+            
             Console.WriteLine("");
 
+        }
+        public static void RetrieveHTML()
+        {
+            var stop =Stopwatch.StartNew();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.youtube.com");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Content length is {0}", response.ContentLength);
+            Console.WriteLine("Content type is {0}", response.ContentType);
+
+            // Get the stream associated with the response.
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format. 
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+            var temp = readStream.ReadToEnd();
+            Console.WriteLine(stop.Elapsed);
+            Console.WriteLine("Response stream received.");
+            Console.WriteLine();
+            response.Close();
+            readStream.Close();
         }
     }
 }
